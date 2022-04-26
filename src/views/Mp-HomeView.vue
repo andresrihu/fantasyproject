1bdac13eea6ce9cb0a011e2a660c7ee5a6581216
Carmen Shannon <carmen.shannon16@gmail.com>
2022-04-26 18:11:18 -0400

    fixed merge

--- src/views/Mp-HomeView.vue
+++ src/views/Mp-HomeView.vue
@@ -7,11 +7,7 @@ export default {
     name: "HomeView",
     data() {
         return {
-<<<<<<< HEAD
             pageTitle: "Hello Andres and the World! We're making it happen!"
-=======
-            pageTitle: "Test 2"
->>>>>>> 74ce721919a535008c8c3246336b6ffb916dc49b
         }
     }
 }
