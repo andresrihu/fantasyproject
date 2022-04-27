5d1b8396ae9c6f4e2f57d6883d4eb7d81d84eb43
Andrés RH <99367051+andresrihu@users.noreply.github.com>
2022-04-27 13:09:33 -0400

    Created PlayerAthletePicks component

---
+++ src/components/PlayerAthletePicks.vue
@@ -0,0 +1,43 @@
+<template>
+  <table class="table table-dark table-hover">
+    <thead>
+      <tr>
+        <th scope="col">Pick</th>
+        <th scope="col">Name</th>
+        <th scope="col">Position</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Jason Doe</td>
+        <td>Quarterback</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Joel Doe</td>
+        <td>Runningback</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>Jane Doe</td>
+        <td>Kicker</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "PlayerAthletePicks",
+  data() {
+    return {
+      
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
