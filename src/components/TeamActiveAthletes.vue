70db29af39aa0a977678928aa849aad567a23028
Carmen Shannon <carmen.shannon16@gmail.com>
2022-04-27 13:50:40 -0400

    added teamactiveathletes

---
+++ src/components/TeamActiveAthletes.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="list-group playercontainer">
+    <rotuer-link class="list-group-item list-group-item-action"
+      >A Player</rotuer-link
+    >
+    <rotuer-link class="list-group-item list-group-item-action"
+      >A Player</rotuer-link
+    >
+    <rotuer-link class="list-group-item list-group-item-action"
+      >A Player</rotuer-link
+    >
+    <rotuer-link class="list-group-item list-group-item-action"
+      >A Player</rotuer-link
+    >
+    <rotuer-link class="list-group-item list-group-item-action"
+      >A Player</rotuer-link
+    >
+    <rotuer-link class="list-group-item list-group-item-action"
+      >A Player</rotuer-link
+    >
+    <rotuer-link class="list-group-item list-group-item-action"
+      >A Player</rotuer-link
+    >
+    <rotuer-link class="list-group-item list-group-item-action"
+      >A Player</rotuer-link
+    >
+    <rotuer-link class="list-group-item list-group-item-action"
+      >A Player</rotuer-link
+    >
+    <rotuer-link class="list-group-item list-group-item-action"
+      >A Player</rotuer-link
+    >
+    <rotuer-link class="list-group-item list-group-item-action"
+      >A Player</rotuer-link
+    >
+    <rotuer-link class="list-group-item list-group-item-action"
+      >A Player</rotuer-link
+    >
+    <rotuer-link class="list-group-item list-group-item-action"
+      >A Player</rotuer-link
+    >
+    <rotuer-link class="list-group-item list-group-item-action"
+      >A Player</rotuer-link
+    >
+    <rotuer-link class="list-group-item list-group-item-action"
+      >A Player</rotuer-link
+    >
+    <rotuer-link class="list-group-item list-group-item-action"
+      >A Player</rotuer-link
+    >
+    <rotuer-link class="list-group-item list-group-item-action"
+      >A Player</rotuer-link
+    >
+    <rotuer-link class="list-group-item list-group-item-action"
+      >A Player</rotuer-link
+    >
+    <rotuer-link class="list-group-item list-group-item-action"
+      >A Player</rotuer-link
+    >
+    <rotuer-link class="list-group-item list-group-item-action"
+      >A Player</rotuer-link
+    >
+    <rotuer-link class="list-group-item list-group-item-action"
+      >A Player</rotuer-link
+    >
+    <rotuer-link class="list-group-item list-group-item-action"
+      >A Player</rotuer-link
+    >
+    <rotuer-link class="list-group-item list-group-item-action"
+      >A Player</rotuer-link
+    >
+  </div>
+</template>
+<script>
+export default {};
+</script>
+
+<style scoped>
+.playercontainer {
+  height: 400px;
+  overflow-y: scroll;
+}
+</style>
