669c0147c2a11c74b410a92850cf5443dc9822c8
Carmen Shannon <carmen.shannon16@gmail.com>
2022-04-27 14:44:18 -0400

    Added AthleteDetailedInfo

---
+++ src/components/AthleteDetailedInfo.vue
@@ -0,0 +1,38 @@
+<template>
+  <table class="table table-dark table-hover">
+    <thead>
+      <tr>
+        <th>Games Played</th>
+        <th>Career Wins</th>
+        <th>Career Losses</th>
+        <th>Career Ties</th>
+        <th>Career Receptions</th>
+        <th>Career Receiving Yards</th>
+        <th>Career Rushing Attempts</th>
+        <th>Career Rushing Yards</th>
+        <th>Career Receiving TD's</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>data</td>
+        <td>data</td>
+        <td>data</td>
+        <td>data</td>
+        <td>data</td>
+        <td>data</td>
+        <td>data</td>
+        <td>data</td>
+        <td>data</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "AthleteDetailedInfo",
+};
+</script>
+
+<style scoped></style>
