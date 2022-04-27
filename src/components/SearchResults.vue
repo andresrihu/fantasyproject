bd96c0e12b2c8b1c4bc3c6064ab120d4ae9324b8
Carmen Shannon <carmen.shannon16@gmail.com>
2022-04-27 12:37:41 -0400

    added SearchResults

---
+++ src/components/SearchResults.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="maincontainer">
+    <div class="leftcontainer list-group">
+      <router-link to="#" class="list-group-item list-group-item-action"
+        >team</router-link
+      >
+      <router-link to="#" class="list-group-item list-group-item-action"
+        >team</router-link
+      >
+      <router-link to="#" class="list-group-item list-group-item-action"
+        >team</router-link
+      >
+      <router-link to="#" class="list-group-item list-group-item-action"
+        >team</router-link
+      >
+      <router-link to="#" class="list-group-item list-group-item-action"
+        >team</router-link
+      >
+      <router-link to="#" class="list-group-item list-group-item-action"
+        >team</router-link
+      >
+      <router-link to="#" class="list-group-item list-group-item-action"
+        >team</router-link
+      >
+      <router-link to="#" class="list-group-item list-group-item-action"
+        >team</router-link
+      >
+      <router-link to="#" class="list-group-item list-group-item-action"
+        >team</router-link
+      >
+      <router-link to="#" class="list-group-item list-group-item-action"
+        >team</router-link
+      >
+    </div>
+    <div class="rightcontainer list-group">
+      <router-link to="#" class="list-group-item list-group-item-action"
+        >player</router-link
+      >
+      <router-link to="#" class="list-group-item list-group-item-action"
+        >player</router-link
+      >
+      <router-link to="#" class="list-group-item list-group-item-action"
+        >player</router-link
+      >
+      <router-link to="#" class="list-group-item list-group-item-action"
+        >player</router-link
+      >
+      <router-link to="#" class="list-group-item list-group-item-action"
+        >player</router-link
+      >
+      <router-link to="#" class="list-group-item list-group-item-action"
+        >player</router-link
+      >
+      <router-link to="#" class="list-group-item list-group-item-action"
+        >player</router-link
+      >
+      <router-link to="#" class="list-group-item list-group-item-action"
+        >player</router-link
+      >
+      <router-link to="#" class="list-group-item list-group-item-action"
+        >player</router-link
+      >
+      <router-link to="#" class="list-group-item list-group-item-action"
+        >player</router-link
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.maincontainer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.leftcontainer,
+.rightcontainer {
+  margin: 5px;
+}
+</style>
