7e2b1e67ce80ca4f27a7ea5f88e947ae99e0b596
Carmen Shannon <carmen.shannon16@gmail.com>
2022-04-27 13:41:41 -0400

    added TeamDetailedInfo added blank activeathletes

---
+++ src/components/TeamDetailedInfo.vue
@@ -0,0 +1,37 @@
+<template>
+  <table class="table table-dark table-hover">
+    <thead>
+      <tr>
+        <th>Year Founded</th>
+        <th>Current Head Coach</th>
+        <th>Career Wins</th>
+        <th>Career Losses</th>
+        <th>Win/Loss Ratio</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>{{ founded }}</td>
+        <td>{{ coach }}</td>
+        <td>{{ wins }}</td>
+        <td>{{ losses }}</td>
+        <td>{{ ratio }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script>
+export default {
+  name: "TeamDetailedInfo",
+  data() {
+    return {
+      founded: "1932",
+      coach: "Bill Belichick",
+      wins: "645",
+      losses: "543",
+      ratio: "1.22",
+    };
+  },
+};
+</script>
+<style scoped></style>
