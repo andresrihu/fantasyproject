8a88141b84c142fd57a3bc331b2d92cfc1b3c0a7
Carmen Shannon <carmen.shannon16@gmail.com>
2022-04-27 18:28:48 -0400

    fixed footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,15 @@
 <template>
-   <div>
-        <div class="footercontainer">
-             <h1>Fantasy Football Website</h1>
-            <div class="bottom">
-                <h5>Created By Andres Rivera Hurtado, Carmen Shannon, John Cunningham, Max Ofosu</h5>
-            </div>
-        </div>
+  <div>
+    <div class="footercontainer">
+      <h1>Fantasy Football Website</h1>
+      <div class="bottom">
+        <h5>
+          Created By Andres Rivera Hurtado, Carmen Shannon, John Cunningham, Max
+          Ofosu
+        </h5>
+      </div>
     </div>
-  
+  </div>
 </template>
 
 <script>
@@ -22,23 +24,23 @@ export default {
 </script>
 
 <style scoped>
-    h1{
-      text-align: center;  
-    }
-    .footercontainer{
-        display: flex; 
-        background-color: rgb(0, 0, 0);
-        color: white;
-        width: 100%;
-        position: fixed;
-        bottom: 0;
-        align-items: center;
-        justify-content: space-around;
-       
-
-    }
-    .bottom{
-        align-self: center;
-        display: flex;
-    }
+h1 {
+  text-align: center;
+}
+.footercontainer {
+  display: flex;
+  background-color: rgb(0, 0, 0);
+  color: white;
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  align-items: center;
+  justify-content: space-around;
+}
+.bottom {
+  align-self: center;
+  display: flex;
+}
 </style>
